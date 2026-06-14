496440a3390a8c75f7e2b31109bd15eb8cb1375b
Shubam Sarawagi <shubam@Shubams-MacBook-Pro.local>
2026-06-14 11:04:44 +0530

    A

--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -304,7 +304,7 @@ export default function Invoices() {
               top: 0;
               width: 100%;
               margin: 0;
-              padding: 0;
+              padding: 48px !important;
               box-shadow: none;
             }
             .bg-white.w-full.max-w-\\[210mm\\] * {
